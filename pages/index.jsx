621d2510c09fa166544a095137bec24411e4c2cd
Korben <korben.au@gmail.com>
2021-11-22 10:54:45 +1100

    add light

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,18 @@
-import {Canvas, useFrame} from "react-three-fiber";
+import {Canvas, useFrame, extend, useThree} from "react-three-fiber";
 import {Box, Container} from "@chakra-ui/react";
 import {useRef} from "react";
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+
+extend({OrbitControls});
 
 export default function Home() {
 
-    const Box3D = () => {
+    const OrbitController = () => {
+        const {camera, gl} = useThree();
+        return <orbitControls args={[camera, gl.domElement]}/>;
+    };
+
+    const TheCube = (props) => {
         const boxRef = useRef();
 
         useFrame(state => {
@@ -13,9 +21,26 @@ export default function Home() {
         });
 
         return (
-            <mesh ref={boxRef}>
+            <mesh castShadow receiveShadow ref={boxRef} {...props}>
                 <boxBufferGeometry/>
-                <meshBasicMaterial color={"blue"}/>
+                <meshPhysicalMaterial color={"blue"}/>
+            </mesh>
+        );
+    };
+
+    const Floor = (props) => {
+        return <mesh {...props} receiveShadow>
+            <boxBufferGeometry args={[20, 0.01, 20]}/>
+            <meshPhysicalMaterial/>
+        </mesh>;
+    };
+
+    const Bulb = (props) => {
+        return (
+            <mesh {...props}>
+                <pointLight castShadow/>
+                <sphereBufferGeometry args={[0.4]}/>
+                <meshPhongMaterial emissive={"yellow"}/>
             </mesh>
         );
     };
@@ -23,12 +48,18 @@ export default function Home() {
     return (
         <Container maxW={"container.xl"}>
             <Box h={"50rem"}>
-                <Canvas style={{background: "black"}}
+                <Canvas style={{background: "gray"}}
+                        shadowMap
                         camera={{
                             position: [3, 3, 3]
                         }}>
-                    {/*<Box3D/>*/}
+                    <fog attach={'fog'} args={["white", 1, 2]}/>
+                    <OrbitController/>
                     <axesHelper args={[5]}/>
+                    <ambientLight intensity={0.1}/>
+                    <Bulb position={[0, 5, 0]}/>
+                    <TheCube position={[1, 2, 1]}/>
+                    <Floor position={[0, 0, 0]}/>
                 </Canvas>
             </Box>
         </Container>
